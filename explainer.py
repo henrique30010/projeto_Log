92ce2df9d95740adb122f5d0e0e82ab9396a0b9a
Ronielle Moreira <ronielle.souza63@aluno.ifce.edu.br>
2025-10-17 18:24:37 -0300

    refac-binarização com z3

--- explainer.py
+++ explainer.py
@@ -1,42 +1,33 @@
 import numpy as np
+from z3 import *
 import time
-from itertools import combinations
-from typing import List, Optional, Set, Tuple
+from typing import Set, Optional
 
-class AbductiveExplanation:
+class AbductiveExplanationZ3:
 
-        def __init__(self, rf_model, domain_theory=None):
-
-            self.model = rf_model
-            self.domain_theory = domain_theory
-            
-        def _get_binary_features(self, X):
-
-            n_features = X.shape[1] #colunas
-            binary_features = []
-            thresholds = []
-            
-            for i in range(n_features):
-                col = X[:, i]
-                
-                threshold = np.median(col)
-                thresholds.append(threshold)
-                binary_features.append(col > threshold)
-                
-            return np.array(binary_features).T, thresholds
+    def __init__(self, rf_model=None):
+        self.model = rf_model
+        self.solver_calls = 0
         
-        def _instance_satisfies_term(self, instance_binary, term_indices):
+    def _binarize_features(self, X):
 
-            return all(instance_binary[i] for i in term_indices)
+        n_features = X.shape[1] #colunas
+        thresholds = [] 
+        X_binary = []
         
-        def _is_abductive_explanation(self, term_indices, x_idx, X_binary, y_pred):
+        for i in range(n_features):
+            col = X[:, i]
+            threshold = np.median(col)
+            thresholds.append(threshold)
+            X_binary.append((col > threshold).astype(int)) #compara com a limiar
             
-            # x_binary base de dados e y_prend lista das previsoes 
-            y_target = y_pred[x_idx]
-            x_binary = X_binary[x_idx] 
-            
-            for idx in range(len(X_binary)):
-                if self._instance_satisfies_term(X_binary[idx], term_indices):
-                    if y_pred[idx] != y_target:
-                        return False
-            return True
+        return np.array(X_binary).T, thresholds
+    
+    def _create_selector_vars(self, ctx, n_features): 
+
+        return [Bool(f's_{i}', ctx) for i in range(n_features)] #vars das caracteristicas
+    
+    def _create_coverage_vars(self, ctx, n_refs): #checklist das referencias
+
+        return [Bool(f'p_{i}', ctx) for i in range(n_refs)]
+   
